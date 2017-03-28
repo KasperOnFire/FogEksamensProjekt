1d6d3c5f571c1d49e08de1fae22d61bf6bb7526c
David Carl <david.martin.carl@gmail.com>
2017-03-28 13:12:35 +0200

    draw class

--- JavaDrawTesting/src/javadrawtesting/JavaDrawTesting.java
+++ JavaDrawTesting/src/javadrawtesting/JavaDrawTesting.java
@@ -1,27 +1,33 @@
 package javadrawtesting;
 
-import java.awt.Graphics;
-import javax.swing. JFrame;
+import java.awt.Color;
+//import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
 
 public class JavaDrawTesting extends JFrame {
 
-    public JavaDrawTesting(){
+    public JavaDrawTesting() {
         setTitle("Testing");
         setSize(960, 960);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    
-    public void paint(Graphics g){
-        g.drawRect(480, 480, 200, 100);
+
+    public void paint() {
+
+    }
+
+    public static void main(String[] args) {
+        //JavaDrawTesting t = new JavaDrawTesting();
+        BufferedImage img = new BufferedImage(500, 500, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = img.createGraphics();
+        g2d.drawRect(200, 200, 100, 200);
+        g2d.dispose();        
+        
+        
         
     }
-    
-    public static void main(String[] args) {
-        JavaDrawTesting t = new JavaDrawTesting();
-        t.paint(null);
 
-
-    }
-    
 }
---
+++ ProjektFog/src/java/Draw/Draw2D.java
@@ -0,0 +1,12 @@
+package Draw;
+
+public class Draw2D {
+
+    public void drawRoof() {
+
+    }
+
+    public void drawSide() {
+
+    }
+}
