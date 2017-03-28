5f7acaa78d63474c0ce5db60664c032645162cbc
kasperonfire <breindal70@gmail.com>
2017-03-28 10:30:30 +0200

    done

--- ProjektFog/src/java/Database/DataAccessObject.java
+++ ProjektFog/src/java/Database/DataAccessObject.java
@@ -1,15 +1,5 @@
 package Database;
 
-import java.io.UnsupportedEncodingException;
-import java.sql.SQLException;
-
 public interface DataAccessObject {
 
-    public int getUIDByUserString(String userString) throws SQLException;
-
-    public void registerUser(String username, String password, String email) throws SQLException, UnsupportedEncodingException;
-
-    public boolean addAlbum(int UID, String artist, String album) throws SQLException;
-
-    public void removeAlbum(String identifier) throws SQLException;
 }
