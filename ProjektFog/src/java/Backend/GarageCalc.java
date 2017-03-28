e96e743ba19d352cdd735fd949e25845b5e53187
CaptainVillain <kristian.krog@gmail.com>
2017-03-28 10:25:46 +0200

    blabla

---
+++ ProjektFog/src/java/Backend/GarageCalc.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Backend;
+
+/**
+ *
+ * @author Kristian
+ */
+public class GarageCalc
+{
+    public int priceCalculator(int length, int width)
+    {
+        return length*width;
+    }
+    
+    
+}
