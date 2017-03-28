a492e0337f00ffd0078acd34f53c9205c90cfe50
kasperonfire <breindal70@gmail.com>
2017-03-28 10:41:12 +0200

    mere db

--- ProjektFog/src/java/Database/DBConnector.java
+++ ProjektFog/src/java/Database/DBConnector.java
@@ -4,22 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-    public class DBConnector {
-        private Connection connection = null;
+public class DBConnector {
 
-        private static final String IP	     = "46.101.97.181";
-        private static final int    PORT     = 3306;
-        public static final String DATABASE  = "collection";
-        private static final String USERNAME = "servletuser";
-        private static final String PASSWORD = "root";	     	
+    private Connection connection = null;
 
-        public DBConnector() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
-                Class.forName("com.mysql.jdbc.Driver").newInstance();
-                String url = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE;
-                this.connection = (Connection) DriverManager.getConnection(url, USERNAME, PASSWORD);
-        }
+    private static final String IP = "46.101.97.181";
+    private static final int PORT = 3306;
+    public static final String DATABASE = "FogDatabase";
+    private static final String USERNAME = "servletuser";
+    private static final String PASSWORD = "ServletSucks";
 
-        public Connection getConnection() {
-                return this.connection;
-        }
+    public DBConnector() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        String url = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE;
+        this.connection = (Connection) DriverManager.getConnection(url, USERNAME, PASSWORD);
     }
+
+    public Connection getConnection() {
+        return this.connection;
+    }
+}
