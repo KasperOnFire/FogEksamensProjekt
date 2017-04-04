bedd819856bc726fe9dfacd047b8528fe4b32d2b
CaptainVillain <kristian.krog@gmail.com>
2017-04-04 12:23:22 +0200

    Updates pre-hashmap

--- ProjektFog/src/java/Backend/MaterialList.java
+++ ProjektFog/src/java/Backend/MaterialList.java
@@ -8,24 +8,49 @@ public class MaterialList
     private int roofBoards;
     private int posts;
     private int postScrews;
-    private int postBands;
+    private int postBandBoards;
+    private double m2;
+    private int roofBoardScrews;
+    private int roofBoardBrackets;
     
-    public void calcRoofScrews(int length,int width)
+    public void calcCarport(int length, int width)
     {
-        double m2 = (double)(length*width)/100;
+        calcRoofScrews(length,width);
+        calcRoofBoards(length);
+        calcPosts(length);
+    }
+    
+    private void calcRoofScrews(int length,int width)
+    {
+        m2 = (double)(length*width)/100;
         roofScrews = (int)m2*12;
     }
     
-    public void calcRoofBoards(int length)
+    private void calcRoofBoards(int length)
     {
         roofBoards = (length/60)+1;
+        roofBoardScrews = roofBoards*18;
+        roofBoardBrackets = roofBoards*2;
     }
     
-    public void calcPosts(int length)
+    private void calcPosts(int length)
     {
-        posts = (length <= 400 ? 2 : (int) Math.ceil((double) length / 200));
+        posts = 2*(length <= 400 ? 2 : (int) Math.ceil((double) length / 200));
         postScrews = posts*4;
-        postBands = posts;
+        postBandBoards = 2;
+    }
+    
+    @Override
+    public String toString()
+    {
+        String ret = "";
+        ret =    "Roofing area(m2): " + m2 + "\n"
+                +"Roof screws: " + roofScrews + "\n"
+                +"Roof boards: " + roofBoards + "\n"
+                +"Posts: " + posts + "\n"
+                +"Post screws: " + postScrews + "\n"
+                +"Post bands: " + postBandBoards;
+        return ret;
     }
     
 }
--- ProjektFog/src/java/Database/DataAccessObjectImpl.java
+++ ProjektFog/src/java/Database/DataAccessObjectImpl.java
@@ -1,11 +1,75 @@
 package Database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DataAccessObjectImpl implements DataAccessObject {
 
-    private final DBConnector conn;
+    private final DBConnector dbcon;
+    private final Connection conn = null;
 
     public DataAccessObjectImpl() throws Exception {
-        this.conn = new DBConnector();
+        this.dbcon = new DBConnector();
     }
 
+    public int getInt(String var, String table, String term, String termName) {
+        String sql = "select ? from ? where ?=?";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, var);
+            stmt.setString(2, table);
+            stmt.setString(3, term);
+            stmt.setString(4, termName);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(var);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DataAccessObjectImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
+    
+    public double getDouble(int id) {
+        String sql = "select * from team where team_id=?";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                int tid = rs.getInt("team_id");
+                String name = rs.getString("teamname");
+                Team returner = new Team(tid, name, getTeamMembers(id));
+                return returner;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DataAccessObjectImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return 0;
+    }
+    
+    public String getString(int id) {
+        String sql = "select * from team where team_id=?";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                int tid = rs.getInt("team_id");
+                String name = rs.getString("teamname");
+                Team returner = new Team(tid, name, getTeamMembers(id));
+                return returner;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DataAccessObjectImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
+    }
 }
