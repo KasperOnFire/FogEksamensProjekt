2405f1a634ad2ac91ec4bc6c42c575640c410722
CaptainVillain <kristian.krog@gmail.com>
2017-05-18 14:25:02 +0200

    cleanup MaterialList

--- ProjektFog/src/java/MaterialList/MaterialList.java
+++ ProjektFog/src/java/MaterialList/MaterialList.java
@@ -1,17 +1,13 @@
 package MaterialList;
 
 import Carport.Carport;
-<<<<<<< HEAD
 import Carport.Roof;
 import Carport.Shed;
-=======
->>>>>>> 3cad4ef6c887df733df5c9e2d17252c3604f5174
 import java.util.HashMap;
 import java.util.Map;
 
 public class MaterialList {
 
-<<<<<<< HEAD
     private int mountingBands = 2;
     private Map<String, Part> matList = new HashMap<>();
     private Part p;
@@ -28,20 +24,7 @@ public class MaterialList {
         DatabaseBack DBB = new DatabaseBack();
         return DBB.getDouble(name);
     }
-
-
-=======
-    private Map<String, Part> matList = new HashMap();
-    private MatRoof matRoof;
-    private MatBase matBase;
-    private MatShed matShed;
     
-    public int calcPrice(Carport c) {
-        calcMaterialList(c);
-        return totalPriceRounded();
-    }
-    
->>>>>>> 3cad4ef6c887df733df5c9e2d17252c3604f5174
     public int totalPriceRounded() {
         double totalPrice = 0;
 
@@ -53,13 +36,8 @@ public class MaterialList {
 
         return (int) totalAdjustedPrice;
     }
-<<<<<<< HEAD
     
     public Map<String, Part> calcMaterialList(Carport c) throws Exception {
-=======
-
-    public Map<String, Part> calcMaterialList(Carport c) {
->>>>>>> 3cad4ef6c887df733df5c9e2d17252c3604f5174
         int length;
 
         if (c.getShed().isHasShed()) {
@@ -67,7 +45,6 @@ public class MaterialList {
         } else {
             length = c.getBase().getDepth() + c.getRoof().getFront() + c.getRoof().getBack();
         }
-<<<<<<< HEAD
         
         int width = c.getBase().getWidth()+c.getRoof().getSides()+c.getRoof().getSides();
         
@@ -77,22 +54,10 @@ public class MaterialList {
         
         if(c.getShed()!=null){
             matList.putAll(calcShed(length, width));
-=======
-
-        int width = c.getBase().getWidth() + c.getRoof().getSides() + c.getRoof().getSides();
-
-        matList.putAll(matRoof.calcRoof(length, width, c.getRoof()));
-
-        matList.putAll(matBase.calcBase(length, width, length, c.getShed()));
-
-        if (c.getShed() != null) {
-            matList.putAll(matShed.calcShed(length, width));
->>>>>>> 3cad4ef6c887df733df5c9e2d17252c3604f5174
         }
 
         return matList;
     }
-<<<<<<< HEAD
     
     public Map<String, Part> calcRoof(int length, int depth, Roof r) throws Exception
     {
@@ -278,6 +243,4 @@ public class MaterialList {
     }
     
 }
-=======
-}
->>>>>>> 3cad4ef6c887df733df5c9e2d17252c3604f5174
+
